1a12c1ec659a1d9af40cc9ffe13ca7af3f62bfe3
C1318676 Cassio Gabriel Costa Dos Santos <c1318676@interno.bb.com.br>
2021-08-20 10:28:54 -0300

    Init

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+
+class App {
+  app = express();
+
+  constructor() {
+    this.app.use(express.json());
+
+    this.app.get('/', (req, res) => {
+      res.send(
+        `Requisições disponíveis: <br><br>
+        /somar: recebe dois parametros: a e b <br>
+        /sqrt: recebe um parametro: a <br><br>
+        Ambos os parametros devem ser passados como queryParams, usando o "?" <br><br>
+        Ex.: <b>localhost:3333/somar?a=1&b=2</b>. Nesse caso o result será 3`
+      );
+    });
+
+    this.app.get('/somar', (req, res) => {
+      const a = parseInt(req?.query?.a as string) || 0;
+      const b = parseInt(req?.query?.b as string) || 0;
+      res.json({ a, b, result: this.somar(a, b) });
+    });
+
+    this.app.get('/sqrt', (req, res) => {
+      const a = parseInt(req?.query?.a as string) || 0;
+      res.json({ a, result: this.sqrt(a) });
+    });
+  }
+
+  somar(a: number, b: number): number {
+    return a + b;
+  }
+
+  sqrt(a: number): number {
+    return Math.sqrt(a);
+  }
+
+}
+
+new App().app.listen(3333);
